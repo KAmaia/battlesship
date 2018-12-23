7ce7f9e75a97c6a331389df2a7dab052f9a8285b
Krystal Lynn Amaia <KrystalAmaia@gmail.com>
2018-12-23 11:17:41 -0500

    Screen size detection working

--- BattlesshipUI.py
+++ BattlesshipUI.py
@@ -4,20 +4,37 @@
 import curses
 from curses import wrapper
 
+def checkSize(screen):
+	if screen.getmaxyx()[0] < 24 or screen.getmaxyx()[1] < 80:
+		return 1
+	else:
+		return 0
 def main(stdscr):
-	##ALL COORDINATES ARE IN Y(LINE),X(COL) FORMAT!!!!
-	stdscr.clear()
-	stdscr.addstr(0,0,"Curses is a trick")
-	lines = curses.LINES
-	stdscr.addstr(1,0,"Lines: " + str(curses.LINES))
-	stdscr.addstr(2,0,"COLS: " + str(curses.COLS))
-	stdscr.refresh()
+	##Check to see if the screen we have is big enough for the entire window.  
+	##Ideally 80x25
+	##If Not Return A Screen Not Big Enough Error, and terminate the session.
+	##If So, go to game Lobby.
+	validTerminal = checkSize(stdscr)	
 
-	
+	if validTerminal == 0:
+		
+		curses.curs_set(0)
+		##ALL COORDINATES ARE IN Y(LINE),X(COL) FORMAT!!!!
+		stdscr.clear()
+		stdscr.addstr(0,0,"Curses is a trick")
+		lines = curses.LINES
+		stdscr.addstr(1,0,"Lines: " + str(curses.LINES))
+		stdscr.addstr(2,0,"COLS: " + str(curses.COLS))
+		stdscr.refresh()
+		gamewin = stdscr.subwin(5,5,5,5)
+		gamewin.box()
+		gamewin.addstr(1,1,"@")
+		gamewin.refresh()
 
-	gamewin = stdscr.subwin(5,5,5,5)
-	gamewin.box()
-	gamewin.addstr(1,1,"@")
-	gamewin.refresh()
+	elif validTerminal == 1:
+		stdscr.clear()
+		stdscr.addstr(0,0,"Sorry, Your Terminal Must Be At Least 80x25 to play")
+		stdscr.refresh()
 	stdscr.getkey()
+##Wrapper##
 wrapper(main)
